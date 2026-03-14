11a3c57cc1d4f8f0cf37deaf75c837fe5dc2ce43
vua22990-boop <vua22990@gmail.com>
2026-03-14 09:53:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("BOT IS RUNNING...")
-bot.infinity_polling()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Shop Quan Ao Tuan Anh"
+
+if __name__ == "__main__":
+    app.run()
