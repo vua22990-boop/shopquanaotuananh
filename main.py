a285c599157f444b3ee17171a3d143b52f558fe5
vua22990-boop <vua22990@gmail.com>
2026-03-14 10:01:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Shop Quan Ao Tuan Anh"
-
-if __name__ == "__main__":
-    app.run()
+    return render_template("index.html")
